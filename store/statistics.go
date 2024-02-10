5986068fd7d0a29ea6024e1ab302a36ce5906c15
Benjamin Heller <benjamin.heller@mein.gmx>
2024-02-10 14:13:05 +0100

    remove comments

--- store/statistics.go
+++ store/statistics.go
@@ -7,14 +7,12 @@ type Count struct {
 
 func (repo Repo) CountCategories() []Count {
 	var categoryCount []Count
-	// repo.db.Debug().Model(&Category{}).Select("count(*) as count, id").Group("id").Scan(&categoryCount)
-	repo.db.Debug().Table("categories_dreams").Select("count(*) as count, category_id as id").Group("category_id").Scan(&categoryCount)
+	repo.db.Table("categories_dreams").Select("count(*) as count, category_id as id").Group("category_id").Scan(&categoryCount)
 	return categoryCount
 }
 
 func (repo Repo) CountPersons() []Count {
 	var categoryCount []Count
-	// repo.db.Debug().Model(&Category{}).Select("count(*) as count, id").Group("id").Scan(&categoryCount)
-	repo.db.Debug().Table("people_dreams").Select("count(*) as count, person_id as id").Group("person_id").Scan(&categoryCount)
+	repo.db.Table("people_dreams").Select("count(*) as count, person_id as id").Group("person_id").Scan(&categoryCount)
 	return categoryCount
 }
