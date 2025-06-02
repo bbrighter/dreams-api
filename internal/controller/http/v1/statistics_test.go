6397ec609df51435cc18ecbfb1bd1fce33af17c9
Benjamin Heller <benjamin.heller@mein.gmx>
2025-06-02 22:08:16 +0200

    ref: add statistic integration test

--- internal/controller/http/v1/statistics_test.go
+++ internal/controller/http/v1/statistics_test.go
@@ -1,34 +1,31 @@
 package v1
 
 import (
-	"net/http/httptest"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/bbrighter/dreams-api/internal/entity"
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 )
 
-type testUseCaseStatistics struct{}
-
-func (t testUseCaseStatistics) GetStatistics(showAll bool, maxNumber int) (entity.Counts, entity.Counts) {
-	var catCounts = entity.Counts{entity.Count{ID: 1, Count: 10}}
-	var perCounts = entity.Counts{entity.Count{ID: 1, Count: 1}, entity.Count{ID: 2, Count: 5}}
-	return catCounts, perCounts
-}
-
-func newTestRouteStatistics() (*statisticsRoute, *gin.Context, *httptest.ResponseRecorder) {
-	var tuc = testUseCaseStatistics{}
-	rec := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(rec)
-	return &statisticsRoute{c: tuc}, c, rec
-}
-
 func TestGetStatistics(t *testing.T) {
-	r, g, rec := newTestRouteStatistics()
+	g := setupApiTest(t)
 
-	r.GetStatistics(g)
-	assert.Equal(t, rec.Code, 200)
-	expectedResp := `{"categories":[{"id":1,"count":10}],"persons":[{"id":1,"count":1},{"id":2,"count":5}]}`
-	assert.Equal(t, rec.Body.String(), expectedResp)
+	tests := []apiTest{
+		{name: "get empty statistics", method: http.MethodGet, url: "/statistics", statusCode: http.StatusOK,
+			response: entity.CountsResponse{Categories: []entity.CountResponse{}, Persons: []entity.CountResponse{}}},
+		{name: "setup dream", method: http.MethodPost, url: "/dreams", statusCode: http.StatusCreated,
+			body: DreamRequestBody{Date: time.Now()}},
+		{name: "setup person", method: http.MethodPut, url: "/dreams/1/persons?name=Pers", statusCode: http.StatusOK},
+		{name: "setup category", method: http.MethodPut, url: "/dreams/1/categories?name=Cat", statusCode: http.StatusOK},
+		{name: "get filled statistics", method: http.MethodGet, url: "/statistics", statusCode: http.StatusOK,
+			response: entity.CountsResponse{
+				Categories: []entity.CountResponse{{ID: 1, Count: 1}},
+				Persons:    []entity.CountResponse{{ID: 1, Count: 1}},
+			}},
+	}
+
+	for _, test := range tests {
+		test.evaluate(t, g)
+	}
 }
