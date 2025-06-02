9efc6a86f78b731d708fbde1258950ac843a6afd
Benjamin Heller <benjamin.heller@mein.gmx>
2025-06-02 22:02:20 +0200

    ref: use more integration tests

--- internal/controller/http/v1/categories_test.go
+++ internal/controller/http/v1/categories_test.go
@@ -1,60 +1,30 @@
 package v1
 
 import (
-	"net/http/httptest"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/bbrighter/dreams-api/internal/entity"
-	"github.com/bbrighter/dreams-api/internal/usecase"
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 )
 
-type mockCategoriesLister struct{}
-type mockCategoriesListerNoResults struct{}
+func TestCategoriesAndPersons(t *testing.T) {
+	g := setupApiTest(t)
+	date := time.Now()
 
-func (tu mockCategoriesLister) List() entity.Categories {
-	return entity.Categories{entity.Category{
-		ID: 1, Name: "Name", Dreams: []entity.Dream{{ID: 10}},
-	}}
-}
-
-func (tu mockCategoriesListerNoResults) List() entity.Categories {
-	return entity.Categories{}
-}
-
-func TestGetAllCategories(t *testing.T) {
-	tests := []struct {
-		name           string
-		uc             usecase.CategoriesLister
-		expectedStatus int
-		expectedBody   string
-	}{
-		{
-			name:           "Ok",
-			uc:             mockCategoriesLister{},
-			expectedStatus: 200,
-			expectedBody:   `{"categories":[{"id":1,"name":"Name"}]}`,
-		},
-		{
-			name:           "no results",
-			uc:             mockCategoriesListerNoResults{},
-			expectedStatus: 200,
-			expectedBody:   `{"categories":[]}`,
-		},
+	tests := []apiTest{
+		{name: "post dream", method: http.MethodPost, url: "/dreams", statusCode: http.StatusCreated,
+			body: DreamRequestBody{Date: date}},
+		{name: "add category to dream", method: http.MethodPut, url: "/dreams/1/categories?name=cat", statusCode: http.StatusOK,
+			response: entity.CategoriesResponse{Categories: []entity.CategoryResponse{{ID: 1, Name: "cat"}}}},
+		{name: "get categories", method: http.MethodGet, url: "/categories", statusCode: http.StatusOK,
+			response: entity.CategoriesResponse{Categories: []entity.CategoryResponse{{ID: 1, Name: "cat"}}}},
+		{name: "remove category from dream", method: http.MethodDelete, url: "/dreams/1/categories/1", statusCode: http.StatusOK,
+			response: entity.CategoriesResponse{Categories: []entity.CategoryResponse{}}},
+		{name: "get categories", method: http.MethodGet, url: "/categories", statusCode: http.StatusOK,
+			response: entity.CategoriesResponse{Categories: []entity.CategoryResponse{}}},
 	}
-
 	for _, test := range tests {
-		gin.SetMode(gin.TestMode)
-		rec := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(rec)
-		route := &categoriesRoute{c: test.uc}
-
-		t.Run(test.name, func(t *testing.T) {
-			route.GetAll(c)
-
-			assert.Equal(t, rec.Code, test.expectedStatus)
-			assert.Equal(t, rec.Body.String(), test.expectedBody)
-		})
+		test.evaluate(t, g)
 	}
 }
---
+++ internal/controller/http/v1/test_helper.go
@@ -0,0 +1,85 @@
+package v1
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/bbrighter/dreams-api/internal/usecase"
+	"github.com/bbrighter/dreams-api/internal/usecase/repository"
+	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+func setupTestDB(t *testing.T) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+	return db
+}
+
+func setupApiTest(t *testing.T) *gin.Engine {
+	logger := zap.L()
+	db := setupTestDB(t)
+	dreamsRepo := repository.NewDreamsRepo(db)
+	dreamsUseCase := usecase.NewDreamUseCase(dreamsRepo)
+	privateDreamsUseCase := usecase.NewPrivateDreamUseCase(dreamsRepo)
+	personsRepo := repository.NewPersonsRepo(db)
+	personsUseCase := usecase.NewPersonsUseCase(personsRepo)
+	categoriesRepo := repository.NewCategoriesRepo(db)
+	categoriesUseCase := usecase.NewCategoriesUseCase(categoriesRepo)
+	statisticsRepo := repository.NewStatisticsRepo(db)
+	statisticsUseCase := usecase.NewStatisticsUseCase(statisticsRepo)
+	repository.Migration(db, logger)
+
+	gin.SetMode(gin.TestMode)
+	handler := gin.New()
+	NewRouter(
+		handler,
+		dreamsUseCase,
+		privateDreamsUseCase,
+		personsUseCase,
+		categoriesUseCase,
+		statisticsUseCase,
+		categoriesUseCase,
+		personsUseCase,
+	)
+	return handler
+}
+
+type apiTest struct {
+	name       string
+	method     string
+	url        string
+	body       any
+	statusCode int
+	response   any
+}
+
+func (test apiTest) evaluate(t *testing.T, h *gin.Engine) {
+	t.Run(test.name, func(t *testing.T) {
+		var body io.Reader
+		if test.body != nil {
+			marBody, _ := json.Marshal(test.body)
+			body = bytes.NewBuffer(marBody)
+		}
+		req, _ := http.NewRequest(test.method, test.url, body)
+		resp := httptest.NewRecorder()
+		h.ServeHTTP(resp, req)
+		assert.Equal(t, test.statusCode, resp.Code)
+
+		if test.response != nil {
+			expected := test.response
+			actual := reflect.New(reflect.TypeOf(expected)).Interface()
+			err := json.Unmarshal(resp.Body.Bytes(), &actual)
+			assert.NoError(t, err)
+			assert.Equal(t, expected, reflect.Indirect(reflect.ValueOf(actual)).Interface())
+		}
+	})
+}
