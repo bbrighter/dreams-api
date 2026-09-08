26f2f4aa0437051ddaea0b37e89b6de788e63250
Benjamin Heller <benjamin.heller@mein.gmx>
2026-09-08 16:14:05 +0200

    fix: Fix test again

--- internal/statistics/repo_test.go
+++ internal/statistics/repo_test.go
@@ -31,7 +31,6 @@ func (s *repoTestSuite) SetupSuite() {
 }
 
 func (s *repoTestSuite) TearDownTest() {
-	s.Equal(1, 2)
 	tables := []string{"categories_dreams", "dreams", "categories"}
 	for _, t := range tables {
 		err := s.db.Exec("DELETE FROM " + t).Error
